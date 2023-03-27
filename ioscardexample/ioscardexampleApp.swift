3b11462de9baae43b62b00765e9c6631f79b54d9
vuslattt <vuslatcoskun2001@gmail.com>
2023-03-27 23:49:14 +0300

    Initial Commit

---
+++ ioscardexample/ioscardexampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ioscardexampleApp.swift
+//  ioscardexample
+//
+//  Created by vuslat coşkun on 27.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct ioscardexampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
